eefd2c8abb8a6077f69a1fbae6a504c5fdd30ba2
beepscore <support@beepscore.com>
2013-10-20 20:58:14 -0700

    Add comment about parameter triangleNumber: maximum value.

--- BSMath/BSMath.h
+++ BSMath/BSMath.h
@@ -30,8 +30,9 @@
      }
 
  @param triangleNumber is an NSUInteger
- If it is zero, the method sets an error.
- TODO: If it is too big for the method to return a valid result, the method sets an error.
+ If it is 0 the method returns 0 and sets an error.
+ If it is greater than or equal to 2**17 = 131072 the method returns 0 and sets an error.
+ The result would be larger than type NSUInteger could hold.
 
  @param errorPointer is type NSError **, the address of an NSError object
  if the method encounters an error, it can dereference &myError to set myError
